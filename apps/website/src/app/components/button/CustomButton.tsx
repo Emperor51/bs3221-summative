ec9e801f5acbf43a11ce374665af80f1e92f5212
Archie S <archie.stevens51@icloud.com>
2025-01-30 21:00:54 +0000

    feature: CustomButton

---
+++ apps/website/src/app/components/button/CustomButton.tsx
@@ -0,0 +1,19 @@
+﻿// CustomButton.tsx
+import React from 'react';
+import { Button, ButtonProps } from 'antd';
+import './CustomButton.css'; // Optional: Import custom styles
+
+interface CustomButtonProps extends ButtonProps {
+  // Add any additional props if needed
+  text?: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Button {...rest} className={`custom-button ${rest.className || ''}`}>
+      {children}
+    </Button>
+  );
+};
+
+export default CustomButton;
