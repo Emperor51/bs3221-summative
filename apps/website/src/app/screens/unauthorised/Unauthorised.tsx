f61822bc49029be72be4b86eb06bfd1e6f6fd7ef
Archie S <archie.stevens51@icloud.com>
2025-02-03 16:29:02 +0000

    feature: :(

--- apps/website/src/app/screens/unauthorised/Unauthorised.tsx
+++ apps/website/src/app/screens/unauthorised/Unauthorised.tsx
@@ -1,9 +1,11 @@
 ﻿import React from 'react';
+import { FrownOutlined } from '@ant-design/icons';
 
 const Unauthorised: React.FC = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2>Unauthorised</h2>
+      <FrownOutlined style={{fontSize: 60}}/>
       <p>You are not authorised to view this page.</p>
     </div>
   );
